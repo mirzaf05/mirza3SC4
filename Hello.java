ba0d5ece4e693a7d4266ae4a8e67494c4a97dbb5
mirzaf05 <77720076+mirzaf05@users.noreply.github.com>
2021-01-21 11:38:19 +0700

    fifth commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("Judul");
 System.out.println("mirzaf05: Aplikasi Java 2020");
 System.out.println("--- *** ---");
+System.out.println("END");
 }
 } 
